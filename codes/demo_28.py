ad6d86bf3517b7ec85c396d53576f988dd6bb22c
WayfaringStranger <shubhampaitrik8@gmail.com>
2020-05-20 17:17:54 +0530

    demo_28

---
+++ codes/demo_28.py
@@ -0,0 +1,24 @@
+import re
+
+dates = """
+01.04.2020
+
+2020.04.01
+
+2020-04-01
+2020-05-23
+2020-06-11
+2020-07-11
+2020-08-11
+
+2020/04/02
+
+2020_04_04
+2020_04_04
+"""
+
+pattern = re.compile(r'\d\d\d\d[-/]0[5-7][-/]\d\d')
+matches = pattern.finditer(dates)
+
+for match in matches:
+    print(match)
